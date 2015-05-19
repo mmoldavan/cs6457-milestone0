31577aa9e4df825dfbb26d13bef283dcbbe5accf
Matthew Moldavan <m.moldavan@gmail.com>
2015-05-19 02:27:47 -0400

    milestone 0

---
+++ Assets/Scripts/Player/PlayerPosioning.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PlayerPosioning : MonoBehaviour
+{
+	public int damagePerShot = 100;
+	public float timeBetweenPosions = 15f;
+
+	float explosionTimer;
+
+	ParticleSystem posionParticles;
+	Light playerLight;
+	float effectsDisplayTime = 0.2f;
+	
+	
+	void Awake ()
+	{
+		posionParticles = GetComponent<ParticleSystem> ();
+		playerLight = GetComponent<Light> ();
+	}
+	
+	
+	void Update ()
+	{
+		explosionTimer += Time.deltaTime;
+		
+		if(Input.GetButton ("Fire1") && explosionTimer >= timeBetweenPosions && Time.timeScale != 0)
+		{
+			Posion ();
+		}
+
+		if(explosionTimer >= timeBetweenPosions * effectsDisplayTime)
+		{
+			DisableEffects ();
+		}
+	}
+	
+	
+	public void DisableEffects ()
+	{
+		playerLight.enabled = false;
+	}
+	
+	
+	void Posion ()
+	{
+		explosionTimer = 0f;
+		
+		playerLight.enabled = true;
+		
+		posionParticles.Stop ();
+		posionParticles.Play ();
+	}
+}
